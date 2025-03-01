59e75c05fe059eab231bc8061a59bf39602b68cf
Alexander Kern <alexander.t.kern@gmail.com>
2025-03-01 14:42:57 -0600

    character counter and testing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter
+from stats import word_counter, character_counter
 
 def get_book_text(filepath): 
     with open(filepath) as f: 
@@ -6,6 +6,7 @@ def get_book_text(filepath):
         return file_contents
 
 def main(): 
-    word_counter(get_book_text("./books/frankenstein.txt") )
+    word_counter(get_book_text("./books/frankenstein.txt"))
+    print(character_counter(get_book_text("./books/frankenstein.txt")))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_counter(str): 
     count = len(str.split())
     print(f"{count} words found in the document")
+
+def character_counter(str): 
+    characters = {}
+    for char in str: 
+        char = char.lower()
+        if char in characters: 
+            characters[char] += 1
+        else: 
+            characters[char] = 1
+    return characters
